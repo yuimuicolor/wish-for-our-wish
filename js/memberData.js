0e2ccf10816757868bd5b81daf4166305c86d16e
yuimuicolor <yuimuicolor@gmail.com>
2025-07-16 14:23:32 +0900

    Initial commit

---
+++ js/memberData.js
@@ -0,0 +1,110 @@
+const memberData = {
+  "sion": {
+    "name": "SION",
+    "gradient": [
+      "#AA80FF",
+      "#7A44E6"
+    ],
+    "textContents": [
+      "시온 내용 1",
+      "시온 내용 2",
+      "시온 내용 3",
+      "시온 내용 4"
+    ],
+    "images": [
+      "./images/ourwish/ourwish-sion-1.png",
+      "./images/ourwish/ourwish-sion-2.png",
+      "./images/ourwish/ourwish-sion-3.png"
+    ]
+  },
+  "riku": {
+    "name": "RIKU",
+    "gradient": [
+      "#F6527B",
+      "#E33560"
+    ],
+    "textContents": [
+      "리쿠 내용 1",
+      "리쿠 내용 2",
+      "리쿠 내용 3",
+      "리쿠 내용 4"
+    ],
+    "images": [
+      "./images/ourwish/ourwish-riku-1.png",
+      "./images/ourwish/ourwish-riku-2.png",
+      "./images/ourwish/ourwish-riku-3.png"
+    ]
+  },
+  "yushi": {
+    "name": "YUSHI",
+    "gradient": [
+      "#35B8EB",
+      "#2D9ECA"
+    ],
+    "textContents": [
+      "유우시 내용 1",
+      "유우시 내용 2",
+      "유우시 내용 3",
+      "유우시 내용 4"
+    ],
+    "images": [
+      "./images/ourwish/ourwish-yushi-1.png",
+      "./images/ourwish/ourwish-yushi-2.png",
+      "./images/ourwish/ourwish-yushi-3.png"
+    ]
+  },
+  "jaehee": {
+    "name": "JAEHEE",
+    "gradient": [
+      "#47F058",
+      "#1ACA2C"
+    ],
+    "textContents": [
+      "재희 내용 1",
+      "재희 내용 2",
+      "재희 내용 3",
+      "재희 내용 4"
+    ],
+    "images": [
+      "./images/ourwish/ourwish-jaehee-1.png",
+      "./images/ourwish/ourwish-jaehee-2.png",
+      "./images/ourwish/ourwish-jaehee-3.png"
+    ]
+  },
+  "ryo": {
+    "name": "RYO",
+    "gradient": [
+      "#DBEB28",
+      "#CDDE19"
+    ],
+    "textContents": [
+      "료 내용 1",
+      "료 내용 2",
+      "료 내용 3",
+      "료 내용 4"
+    ],
+    "images": [
+      "./images/ourwish/ourwish-ryo-1.png",
+      "./images/ourwish/ourwish-ryo-2.png",
+      "./images/ourwish/ourwish-ryo-3.png"
+    ]
+  },
+  "sakuya": {
+    "name": "SAKUYA",
+    "gradient": [
+      "#FF5ACB",
+      "#EF00A4"
+    ],
+    "textContents": [
+      "사쿠야 내용 1",
+      "사쿠야 내용 2",
+      "사쿠야 내용 3",
+      "사쿠야 내용 4"
+    ],
+    "images": [
+      "./images/ourwish/ourwish-sakuya-1.png",
+      "./images/ourwish/ourwish-sakuya-2.png",
+      "./images/ourwish/ourwish-sakuya-3.png"
+    ]
+  }
+};
